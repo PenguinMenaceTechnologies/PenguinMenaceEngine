8dc446cc9ec3b859db3b4103f29305a0ef218bfe
penguinmenac3 <penguinmenac3@gmail.com>
2014-03-10 00:04:02 +0100

    Added a flush for offscreen rendering.

--- src/main/java/net/pme/GameLoop.java
+++ src/main/java/net/pme/GameLoop.java
@@ -225,7 +225,8 @@ final class GameLoop {
 					for (Particle o : particleObjects) {
 						o.render();
 					}
-	                
+
+                    GL11.glFlush();
 	                tmp.postRender();
 	            }
 
