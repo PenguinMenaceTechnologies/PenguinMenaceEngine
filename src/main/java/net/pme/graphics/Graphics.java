6194ee521315b1eb9b884671aae9735b6ef18544
penguinmenac3 <penguinmenac3@gmail.com>
2014-03-10 15:17:24 +0100

    Minor beauty fix. There was still the debug gray in offscreen rendering.

--- src/main/java/net/pme/graphics/Graphics.java
+++ src/main/java/net/pme/graphics/Graphics.java
@@ -435,7 +435,7 @@ public final class Graphics {
             glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
         }
 
-        GL11.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT
                 | GL11.GL_DEPTH_BUFFER_BIT);
 
