bb2f46273977a8da65f9f97e4918a30aa493cf65
penguinmenac3 <penguinmenac3@gmail.com>
2014-05-13 01:03:58 +0200

    Added missing buffer clearing.

--- src/main/java/net/pme/graphics/Graphics.java
+++ src/main/java/net/pme/graphics/Graphics.java
@@ -386,6 +386,10 @@ public final class Graphics {
             synchronized (this) {
                 for (OffscreenRendererWrapper tmp : offscreenRenderers) {
                     if (tmp.preRender(game)) {
+                        GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+                        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT
+                                | GL11.GL_DEPTH_BUFFER_BIT);
+
                         // Render all objects
                         for (GameObject o : objects) {
                             if (o.getRenderAttachment() != null) {
